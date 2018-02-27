925506d665bd69a9a5b9e6c22e70290b59c48171
Miloslav Homer <miloslav.homer@gmail.com>
2018-02-27 10:17:24 +0100

    PoC in best conditions - one enc query, 256 dec queries.

--- attack.c
+++ attack.c
@@ -1,8 +1,9 @@
-/* proof of concept of the attack on OPP
+/* Proof of Concept of the Attack on OPP
 
     :copyright: (c) 2018 by Miloslav Homer
     :license: Creative Commons CC0 1.0
 */
+
 /*
     MEM AEAD source code package
 
@@ -12,6 +13,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
+/* constants holding number of bytes for two blocks and a byte and one block and a byte */
+#define TWO_AND_BYTE 257 
+#define ONE_AND_BYTE 129
 
 void crypto_aead_encrypt(
         unsigned char *c, size_t *clen,
@@ -27,31 +32,21 @@ int crypto_aead_decrypt(
         const unsigned char *nonce,
         const unsigned char *key);
 
-void attackPoC()
+int attackPoC(
+		const unsigned char *h, size_t hlen, 
+		const unsigned char *n, 
+		const unsigned char *k)
 {
-#define TWO_AND_BYTE 257 
-#define ONE_AND_BYTE 129
-    unsigned char h[TWO_AND_BYTE];
-    unsigned char k[32];
-    unsigned char n[16];
-
     unsigned i;
+	int success = 0;
 
+	/* initialize variables holding plaintext and forgery */
     unsigned char m[TWO_AND_BYTE];
     unsigned char c[TWO_AND_BYTE + 32];
 	unsigned char f[ONE_AND_BYTE + 32];
     size_t mlen;
     size_t clen;
-    size_t hlen;
 	size_t flen;
-
-    for(i = 0; i < sizeof h; ++i)
-        h[i] = 255 & (i*193 + 123);
-    for(i = 0; i < sizeof k; ++i)
-        k[i] = 255 & (i*191 + 123);
-    for(i = 0; i < sizeof n; ++i)
-        n[i] = 255 & (i*181 + 123);
-
     memset(m, 0, sizeof m);
 	memset(f, 0, sizeof f);
 	clen = 0;
@@ -61,14 +56,13 @@ void attackPoC()
 	/* make query */
     crypto_aead_encrypt(c, &clen, h, hlen, m, mlen, n, k);
 
+	/* null plaintext for decryption purposes */
     memset(m, 0, sizeof m);
     mlen = 0;
 
 	/* make forgery */
 	memcpy(f, c, ONE_AND_BYTE);
-	for (i = 0; i < 32; ++i) {
-		f[ONE_AND_BYTE+i] = c[TWO_AND_BYTE+i];
-	}
+	memcpy(f+ONE_AND_BYTE, c+TWO_AND_BYTE, 32);
 
 	/* cycle through possible last bytes */
 	for (i = 0; i<256; i++) {
@@ -76,18 +70,29 @@ void attackPoC()
 		
 		/* test forgery */
     	if( 0 == crypto_aead_decrypt(m, &mlen, h, hlen, f, flen, n, k) ) {
-			printf("Got em!\n");	
+			success = 1;
 			break;
 		}
 	}
-
-#undef TWO_AND_BYTE
-#undef ONE_AND_BYTE
+	return success;
 }
 
 int main()
 {
-	attackPoC();
+	/* choose nonce, key, header randomly */
+	unsigned i;
+    unsigned char h[TWO_AND_BYTE];
+    unsigned char k[32];
+    unsigned char n[16];
+    for(i = 0; i < sizeof h; ++i) h[i] = 255 & (i*193 + 123);
+    for(i = 0; i < sizeof k; ++i) k[i] = 255 & (i*191 + 123);
+    for(i = 0; i < sizeof n; ++i) n[i] = 255 & (i*181 + 123);
+	
+	if (attackPoC(h, sizeof h, n, k)) printf("Got 'em!\n");
+	else printf("Invalid forgery :(\n");
+
 	return 0;
 }
 
+#undef TWO_AND_BYTE
+#undef ONE_AND_BYTE
